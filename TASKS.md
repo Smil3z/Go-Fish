4954236fbcd5114b55cfd51d29baf71819b3548f
Stephon Tonge <stephon.tonge75@gmail.com>
2023-12-21 23:54:54 -0600

    created database and env file

---
+++ TASKS.md
@@ -0,0 +1,12 @@
+# Tasks
+
+// TODO!
+
+[] Create database
+[] Route for login page
+[] Route for register page
+[] Customize login page
+[] Customize register page
+[] Customize home page
+[] Route for details page
+[] Route for Add/Edit page
--- database.sql
+++ database.sql
@@ -3,8 +3,21 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "fishes" (
+  "id" integer [primary key],
+  "user_id" integer,
+  "name" varchar,
+  "location" varchar,
+  "image_url" varchar,
+  "description" varchar,
+  "caught_at" TIMESTAMP,
+  "length" varchar,
+  "weight" varchar
 );
