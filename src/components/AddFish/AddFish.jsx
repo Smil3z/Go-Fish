893084137b2c5a754e10e937689a0408ab2fe54b
Stephon Tonge <stephon.tonge75@gmail.com>
2024-01-17 10:57:10 -0600

    added form for add fish page

--- src/components/AddFish/AddFish.jsx
+++ src/components/AddFish/AddFish.jsx
@@ -2,6 +2,17 @@ import { useDispatch, useSelector} from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
 
 function AddFish() {
     const add = useSelector( store => store.add );
@@ -26,9 +37,29 @@ function AddFish() {
     },[]);
 
     return (
-        <>
-            <h2>Add Fish</h2>
-        </>
+        <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+            
+            <div>
+                <h2> Add Fish</h2>
+                <br />
+                <TextField label="Name" id="outlined-start-adornment" sx={{ m: 1, width: '25ch' }}/>
+                <br />
+                <TextField label="Location" id="outlined-start-adornment" sx={{ m: 1, width: '25ch' }}/>
+                <br />
+                <TextField label="Image url" id="outlined-start-adornment" sx={{ m: 1, width: '25ch' }}/>
+                <br />
+                <TextField label="Description" id="outlined-start-adornment" sx={{ m: 1, width: '25ch' }}/>
+                <br />
+                <TextField label="Caught at" id="outlined-start-adornment" sx={{ m: 1, width: '25ch' }}/>
+                <br />
+                <TextField label="Length" id="outlined-start-adornment" sx={{ m: 1, width: '25ch' }}/>
+                <br />
+                <TextField label="Weight" id="outlined-start-adornment" sx={{ m: 1, width: '25ch' }}/>
+                <br />
+                <br />
+                <Button variant="contained"> Add </Button>
+            </div>
+        </Box>
     );
 }
 
